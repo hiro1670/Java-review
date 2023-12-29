ea460425363915bc1eece2fee7e5a6130d4db543
hiro1670 <hiroto0414@ezweb.ne.jp>
2023-12-29 16:17:12 +0900

    [Finish]1章 練習問題

---
+++ java-review/practice1/Practise2.java
@@ -0,0 +1,14 @@
+package practice1;
+
+public class Practise2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		boolean tama = true;
+		char koma = '駒';
+		double ensyuuritu = 3.14;
+		long Pi = 314159265853979L;
+		String damage = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+	}
+
+}
