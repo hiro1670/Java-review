9f7d3ca3070453b5e59afc74ba558ae10f3ba63c
hiro1670 <hiroto0414@ezweb.ne.jp>
2023-12-29 15:42:48 +0900

    [Finish]1.2 Javaプログラムの基本構造

---
+++ java-review/capter1/main.java
@@ -0,0 +1,13 @@
+package capter1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("RPG:スッキリ魔王討伐");
+		System.out.println("Ver.0.1 by 湊");
+		System.out.println("＜ただいま鋭意学習・製作中＞");
+		System.out.println("プログラムを終了します");
+	}
+
+}
