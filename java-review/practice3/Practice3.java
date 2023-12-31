5e0dee9c512201dc3bf1b4ebab05efc2ecb6ef64
hiro1670 <hiroto0414@ezweb.ne.jp>
2023-12-31 14:55:09 +0900

    [Finish]3-4 練習問題

---
+++ java-review/practice3/Practice3.java
@@ -0,0 +1,17 @@
+package practice3;
+
+public class Practice3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		boolean tenki = false;
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩にいきます");
+		} else {
+			System.out.println("映画を見ます");
+			System.out.println("寝ます");
+		}
+	}
+
+}
