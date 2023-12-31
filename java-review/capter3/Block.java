8ad785267164c178705c67f2befe92316e239a88
hiro1670 <hiroto0414@ezweb.ne.jp>
2023-12-31 13:23:05 +0900

    [Finish]ブロックの書き方

---
+++ java-review/capter3/Block.java
@@ -0,0 +1,15 @@
+package capter3;
+
+public class Block {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		boolean tenki = true;
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩にいきます");
+		} else 
+			System.out.println("映画を見ます");
+	}
+
+}
