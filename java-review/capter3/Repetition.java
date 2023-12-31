bf678e35d71d6831fbed1ad5a10a4b23c00335e9
hiro1670 <hiroto0414@ezweb.ne.jp>
2023-12-31 13:12:12 +0900

    [Finish]3章 while文-繰り返し

---
+++ java-review/capter3/Repetition.java
@@ -0,0 +1,14 @@
+package capter3;
+
+public class Repetition {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		boolean doorClose = false;
+		while (doorClose == true) {
+			System.out.println("ノックする");
+			System.out.println("1分待つ");
+		}
+	}
+
+}
