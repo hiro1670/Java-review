bbcfe01dc33ef3847c4a29432e0497a088fc471d
hiro1670 <hiroto0414@ezweb.ne.jp>
2023-12-31 14:46:52 +0900

    [Finish]3-4 練習問題

---
+++ java-review/practice3/Practice2.java
@@ -0,0 +1,19 @@
+package practice3;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int isHungry = 1;
+		String food = "ドリア";
+		System.out.println("こんにちは");
+		if (isHungry == 0) {
+			System.out.println("お腹がいっぱいです");
+		} else {
+			System.out.println("はらぺこです");
+			System.out.println(food + "をいただきます");
+		}
+		System.out.println("ごちそうさまでした");
+	}
+
+}
