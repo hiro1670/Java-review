b457ac142c11b369d1c0b7aa4ee1a6c1494ba880
hiro1670 <hiroto0414@ezweb.ne.jp>
2023-12-31 13:52:33 +0900

    [Finish]for文

---
+++ java-review/capter3/For.java
@@ -0,0 +1,12 @@
+package capter3;
+
+public class For {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int i = 0; i < 3; i++) {
+			System.out.println("現在" + (i + 1) + "周目");
+		}
+	}
+
+}
