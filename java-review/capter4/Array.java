10bfdb21300f221ddc4d2fbbfd4527e11312eeb7
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-01-02 12:28:20 +0900

    [Finish]4章 配列

---
+++ java-review/capter4/Array.java
@@ -0,0 +1,18 @@
+package capter4;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int sansu = 20;
+		int kokugo = 30;
+		int rika = 40;
+		int eigo = 50;
+		int syakai = 80;
+		int sum = sansu + kokugo + rika + eigo + syakai;
+		int avg = sum / 5;
+		System.out.println("合計点：" + sum);
+		System.out.println("平均点：" + avg);
+	}
+
+}
---
+++ java-review/capter4/ArrayValue.java
@@ -0,0 +1,12 @@
+package capter4;
+
+public class ArrayValue {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] scores = new int[5];
+		scores[1] = 30;
+		System.out.println(scores[1]);
+	}
+
+}
