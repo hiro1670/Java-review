36d15fa728de3c80b9eabf5f4d264643bf4e59cd
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-01-02 12:51:47 +0900

    [Finish]4章 ループによる集計

---
+++ java-review/capter4/Array_for.java
@@ -0,0 +1,13 @@
+package capter4;
+
+public class Array_for {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] scores = {20, 30, 40, 50, 80};
+		for (int i = 0; i < scores.length; i++) {
+			System.out.println(scores[i]);
+		}
+	}
+
+}
