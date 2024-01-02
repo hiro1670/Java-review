1ad8e6d58368dae46f19e3fcbd1bec22a4afc17f
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-01-02 13:14:13 +0900

    [Finish]4.4 配列のデータをまとめて扱う

---
+++ java-review/capter4/Expansion_for.java
@@ -0,0 +1,13 @@
+package capter4;
+
+public class Expansion_for {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] scores = {20, 30, 40, 50, 80};
+		for (int value : scores) {
+			System.out.println(value);
+		}
+	}
+
+}
