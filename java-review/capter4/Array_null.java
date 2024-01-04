88aa124776feee32ce6a963bd499613e2be991b3
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-01-04 13:53:24 +0900

    [Finish]4.6 配列の後片付け

---
+++ java-review/capter4/Array_null.java
@@ -0,0 +1,12 @@
+package capter4;
+
+public class Array_null {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int [] array = {1, 2, 3};
+		array = null;
+		array[0] = 10;
+	}
+
+}
---
+++ java-review/capter4/GarbageCollection.java
@@ -0,0 +1,13 @@
+package capter4;
+
+public class GarbageCollection {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		boolean judge = true;
+		if (judge == true) {
+			int[] array = {1, 2, 3};
+		}
+	}
+
+}
