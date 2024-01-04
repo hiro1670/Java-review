ae0d3d4542679bc97134d5e0765c5aeae35a8744
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-01-04 12:28:05 +0900

    [Finish]4.5 配列の舞台裏

---
+++ java-review/capter4/Array_rikai.java
@@ -0,0 +1,14 @@
+package capter4;
+
+public class Array_rikai {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] arrayA = {1, 2, 3};
+		int[] arrayB;
+		arrayB = arrayA;
+		arrayB[0] = 100;
+		System.out.println(arrayA[0]);
+	}
+
+}
