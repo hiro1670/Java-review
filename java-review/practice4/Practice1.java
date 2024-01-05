15d94dfaf475587f7b3c71197ab3259e7cbd0039
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-01-05 18:08:09 +0900

    [Finish]4-1 練習問題

---
+++ java-review/practice4/Practice1.java
@@ -0,0 +1,24 @@
+package practice4;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] points = new int[4];
+		
+		double[] weights = new double[5];
+		
+		boolean [] answers = new boolean[3];
+		
+		String [] names = new String[3];
+		
+		System.out.println(points.length);
+		
+		System.out.println(weights.length);
+		
+		System.out.println(answers.length);
+		
+		System.out.println(names.length);
+	}
+
+}
