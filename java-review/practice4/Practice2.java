99dd39502ec1037e84b33949d941cbc548ad1428
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-01-05 18:23:21 +0900

    [Finish]4-2 練習問題

---
+++ java-review/practice4/Practice2.java
@@ -0,0 +1,19 @@
+package practice4;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] moneyList = {121902, 8302, 55100};
+		for (int i = 0; i < moneyList.length; i++) {
+			System.out.println(moneyList[i]);
+		}
+		
+		System.out.println("");
+		
+		for (int value : moneyList) {
+			System.out.println(value);
+		}
+	}
+
+}
