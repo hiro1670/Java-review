dc4dcb8bd460413a2b0ef957aefab48ec6dcb9dc
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-01-05 17:58:55 +0900

    [Finish]4.7 多次元の配列

---
+++ java-review/capter4/ParentAndChild_Array.java
@@ -0,0 +1,12 @@
+package capter4;
+
+public class ParentAndChild_Array {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[][] scores = {{40, 50, 60}, {80, 60, 70}};
+		System.out.println(scores.length);
+		System.out.println(scores[0].length);
+	}
+
+}
