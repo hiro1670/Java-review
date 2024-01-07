cd816bf3abbf903ed16190f6cd0d7db6a90f4130
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-01-07 16:42:21 +0900

    [Finish]5.4 オーバーロードの利用

---
+++ java-review/capter5/Overload_Number.java
@@ -0,0 +1,19 @@
+package capter5;
+
+public class Overload_Number {
+
+	public static int add(int x, int y) {
+		return x + y;
+	}
+	
+	public static int add(int x, int y, int z) {
+		return x + y + z;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("10 + 20 = " + add(10, 20));
+		
+		System.out.println("10 + 20 + 30 = " + add(10, 20, 30));
+	}
+
+}
