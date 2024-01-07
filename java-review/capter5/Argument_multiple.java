a22511e212a46eaf59651692ecf498b665d759d6
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-01-07 16:03:08 +0900

    [Finish]5.2 引数の利用

--- java-review/capter5/Argument_multiple.java
+++ java-review/capter5/Argument_multiple.java
@@ -10,7 +10,7 @@ public class Argument_multiple {
 	
 	public static void add(int x, int y) {
 		int ans = x + y;
-		System.out.println(x + "+" + y + "=" +  ans);
+		System.out.println(x + " + " + y + " = " + ans);
 	}
 
 }
