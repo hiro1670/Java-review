f5d1910d4a7f569d329a8ed538b4d3562993a366
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-01-07 15:39:02 +0900

    [Finish]5.1.4 mainメソッド以外からのメソッドの呼び出し

---
+++ java-review/capter5/MainOtherthan_Call.java
@@ -0,0 +1,18 @@
+package capter5;
+
+public class MainOtherthan_Call {
+
+	public static void methodA() {
+		System.out.println("methodA");
+		methodB();
+	}
+	
+	public static void methodB() {
+		System.out.println("methodB");
+	}
+	
+	public static void main(String[] args) {
+		methodA();
+	}
+
+}
