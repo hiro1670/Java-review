014405ec60e3d256603320cd2b942e3cba2652ff
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-01-07 15:09:28 +0900

    [Finish]4-4 練習問題

--- java-review/practice4/Practice3.java
+++ java-review/practice4/Practice3.java
@@ -4,6 +4,14 @@ public class Practice3 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		int[] numbers = {3, 4, 9};
+		System.out.println("1桁の数字を入力してください");
+		int input = new java.util.Scanner(System.in).nextInt();
+		for (int i = 0; i < numbers.length; i++) {
+			if (input == numbers[i]) {
+				System.out.println("アタリ！");
+			}
+		}
 	}
 
 }
