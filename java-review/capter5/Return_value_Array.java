29df9e2c9227bd33cfdf8829e5f0db007f79a6c2
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-01-08 18:26:46 +0900

    [Finish]5.5 引数や戻り値に配列を用いる

---
+++ java-review/capter5/Return_value_Array.java
@@ -0,0 +1,18 @@
+package capter5;
+
+public class Return_value_Array {
+	public static int[] makeArray(int size) {
+		int[] newArray = new int[size];
+		for (int i = 0; i < newArray.length; i++) {
+			newArray[i] = i;
+		}
+		return newArray;
+	}
+	
+	public static void main(String[] args) {
+		int[] array = makeArray(3);
+		for (int i : array) {
+			System.out.println(i);
+		}
+		}
+}
