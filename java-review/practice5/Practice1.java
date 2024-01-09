5d03555f1ca9b3a3081c47f5550792726ed27389
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-01-09 17:53:11 +0900

    [Finish]5-1 練習問題

--- java-review/practice5/Practice1.java
+++ java-review/practice5/Practice1.java
@@ -2,9 +2,27 @@ package practice5;
 
 public class Practice1 {
 
+	public static void introduceOneself(String name) {
+		System.out.println("私の名前は" + name + "です。");
+	}
+	
+	public static void introduceOneself(int age) {
+		System.out.println("歳は" + age + "歳です。");
+	}
+	
+	public static void introduceOneself(double height) {
+		System.out.println("身長は" + height + "cmです。");
+	}
+	
+	public static void introduceOneself(char zodiac) {
+		System.out.println("十二支は" + zodiac + "です。");
+	}
+	
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		introduceOneself("玉井裕人");
+		introduceOneself(23);
+		introduceOneself(167.7);
+		introduceOneself('辰');
 	}
 
 }
