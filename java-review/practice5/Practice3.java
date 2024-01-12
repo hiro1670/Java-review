fa026292953f76ff61956620b1580b32fb0fc5c7
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-01-12 18:03:24 +0900

    [Finish]5-3 練習問題

--- java-review/practice5/Practice3.java
+++ java-review/practice5/Practice3.java
@@ -1,5 +1,14 @@
 package practice5;
 
 public class Practice3 {
-
+	
+	public static void email(String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました");
+		System.out.println("件名：無題");
+		System.out.println("本文：" + text);
+	}
+	
+	public static void main(String[] args) {
+		email("hiro@.com", "朝ごはん食べた？");
+	}
 }
