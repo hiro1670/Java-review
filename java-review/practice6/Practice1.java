1dbfe9ddd845839560461f873b01dbeb24e45fac
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-01-14 17:34:55 +0900

    [Finish]6-1 練習問題

---
+++ java-review/practice6/Practice1.java
@@ -0,0 +1,13 @@
+package practice6;
+
+public class Practice1 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		Kouhan.callDeae();
+		Kouhan.showMondokoro();
+	}
+
+}
